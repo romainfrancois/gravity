958a7d862ca924d0da3f6c128788a6095bf47a15
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-10-22 10:40:17 -0300

    v0.9.0 with BVU in vignettes

--- vignettes/crash-course-on-gravity-models.R
+++ vignettes/crash-course-on-gravity-models.R
@@ -22,3 +22,33 @@ tidy(fit)
 glance(fit)
 augment(fit)
 
+## ----bvu-----------------------------------------------------------------
+fit2 <- bvu(
+    dependent_variable = "flow",
+    distance = "distw",
+    additional_regressors = c("rta", "contig", "comcur"),
+    income_origin = "gdp_o",
+    income_destination = "gdp_d",
+    code_origin = "iso_o",
+    code_destination = "iso_d",
+    data = gravity_no_zeros
+  )
+
+tidy(fit2)
+glance(fit2)
+
+## ----bvw-----------------------------------------------------------------
+fit3 <- bvw(
+    dependent_variable = "flow",
+    distance = "distw",
+    additional_regressors = c("rta", "comcur", "contig"),
+    income_origin = "gdp_o",
+    income_destination = "gdp_d",
+    code_origin = "iso_o",
+    code_destination = "iso_d",
+    data = gravity_no_zeros
+  )
+
+tidy(fit3)
+glance(fit3)
+
